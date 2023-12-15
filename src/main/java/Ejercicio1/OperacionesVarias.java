64461316c18c28cc4478668657c8e36e7768f0ff
JorgeNavVel <jnv309@educa.madrid.org>
2023-12-15 10:57:14 +0100

    added case 5

--- src/main/java/Ejercicio1/OperacionesVarias.java
+++ src/main/java/Ejercicio1/OperacionesVarias.java
@@ -1,4 +1,59 @@
 package Ejercicio1;
 
 public class OperacionesVarias {
+    public void comprobarperfecto(int num) {
+
+        int sumadivisores = 0;
+        for (int i = 1; i < num; i++) {
+            if (num%i == 0) {
+                sumadivisores = sumadivisores + i;
+            }
+        }
+        if (sumadivisores == num) {
+            System.out.println("El numero es perfecto.");
+        }
+    }
+    public void comprobarprimo(int num) {
+
+        boolean primo = false;
+        for (int i = 2; i <= num/2 ; i++) {
+            if (num%i == 0) {
+                primo = true;
+                break;
+            }
+        }
+        if (!primo) {
+            System.out.println("Es un numero primo");
+        }
+        else {
+            System.out.println("No es un numero primo");
+        }
+    }
+
+    public void comprobarabundante(int num) {
+
+        int suma = 0;
+        for (int i = 1; i < num; i++) {
+            if (num%i == 0) {
+                suma = suma + i;
+            }
+        }
+        if (suma > num) {
+            System.out.println("Es un numero abundante");
+        }
+        else {
+            System.out.println("No es un numero abundante");
+        }
+    }
+
+    public void piramide(int num) {
+
+        for (int i = 1; i <= num ; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/main/java/Ejercicio1/Tester.java
+++ src/main/java/Ejercicio1/Tester.java
@@ -1,4 +1,39 @@
 package Ejercicio1;
 
+import java.util.Scanner;
+
 public class Tester {
+    public static void main(String[] args) {
+
+        int opcion;
+        int numerito;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca una opcion\n[1] Saber si un numero es perfecto\n[2] Saber si un numero es primo\n[3] Comprobar si un numero es abundante\n[4] Construir pirámide");
+        opcion = teclado.nextInt();
+        OperacionesVarias operacionesvarias = new OperacionesVarias();
+
+        switch (opcion) {
+            case 1:
+                System.out.println("Introduzca un numero");
+                numerito = teclado.nextInt();
+                operacionesvarias.comprobarperfecto(numerito);
+
+            case 2:
+                System.out.println("Introduzca un numero");
+                numerito = teclado.nextInt();
+                operacionesvarias.comprobarprimo(numerito);
+
+            case 3:
+                System.out.println("Introduzca un numero");
+                numerito = teclado.nextInt();
+                operacionesvarias.comprobarabundante(numerito);
+
+            case 4:
+                System.out.println("Introduzca un numero");
+                numerito = teclado.nextInt();
+                operacionesvarias.piramide(numerito);
+
+            case 5:
+        }
+    }
 }
